42596937969c9f87416a8ffde6a7d8d746f4b7d9
EpicRaisin <gabe@muldowneyclan.com>
2022-11-13 14:23:17 -0600

    no frame

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = electron.app
 app.on("ready", () => {
     const window = new electron.BrowserWindow({
         fullscreen: true,
+        frame: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
